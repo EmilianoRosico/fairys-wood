7c05adaad45ac776b4147316038e3c8c5f37a925
Emiliano Rosico <emiliano19@gmail.com>
2024-03-31 13:27:52 -0300

    change the import

--- components/useLocales.js
+++ components/useLocales.js
@@ -6,7 +6,7 @@ export const useLocales = (path) => {
    const [text, setText] = useState({});
    useEffect(() => {
       if (path) {
-         import(`${process.cwd()}/lib/${path}/${locale}`).then((t) => {
+         import(`../lib/${path}/${locale}`).then((t) => {
          setText(t.default);
       });
    }
